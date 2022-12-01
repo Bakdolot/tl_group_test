5530530323e579e55c2202e5bd7d3a95d6650b86
Bakdolot <genry03gh@gmail.com>
2022-12-01 18:01:27 +0600

    init

---
+++ main/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class DivisionLevel(models.IntegerChoices):
+    LEVEL_1 = 1
+    LEVEL_2 = 2
+    LEVEL_3 = 3
+    LEVEL_4 = 4
+    LEVEL_5 = 5
+
+
+class Division(models.Model):
+    name = models.CharField(max_length=100)
+    level = models.PositiveSmallIntegerField(choices=DivisionLevel.choices)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Employer(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    acceptance_date = models.DateTimeField(auto_now_add=True)
+    salary = models.DecimalField(max_digits=9, decimal_places=2)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
